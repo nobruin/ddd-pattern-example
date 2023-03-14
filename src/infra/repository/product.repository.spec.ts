5a8845e03a95845652b5b1ecb446ebfe47cdf554
ext_brsalis <ext_brsalis@mercadolibre.com>
2023-03-14 09:44:16 -0300

    create database infrastructure

---
+++ src/infra/repository/product.repository.spec.ts
@@ -0,0 +1,22 @@
+import { Sequelize } from "sequelize-typescript"
+import ProductModel from "../db/sequelize/model/product.model"
+
+describe("Product repository test", () => {
+    let sequilize: Sequelize
+
+    beforeEach( async () => {
+        sequilize =  new Sequelize({
+            dialect: 'sqlite',
+            storage: 'memory',
+            logging: false,
+            sync: {force:true}
+        })
+        
+        sequilize.addModels([ProductModel])
+        await sequilize.sync()
+        
+        afterEach(async () => {
+            await sequilize.close()
+        })
+    })
+})
